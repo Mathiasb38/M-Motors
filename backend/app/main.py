064b7e2d0e20133885138ca0bbd312a6082227c5
Mathiasb38 <mb@telegrenoble.net>
2026-08-04 12:33:05 +0200

    ci: frontend deployments

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://m-motors-frontend-hxk4.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
