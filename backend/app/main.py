7b0069517ec56f7a14ae42fd5f4c0c905a799f98
Mathiasb38 <mb@telegrenoble.net>
2026-08-06 14:15:43 +0200

    feat: connect database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database import check_database_connection, close_database_connection
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    check_database_connection()
+    yield
+    close_database_connection()
+
 
 app = FastAPI(
-    title="M-motors API"
+    title="M-motors API",
+    lifespan=lifespan,
 )
 
 app.add_middleware(
